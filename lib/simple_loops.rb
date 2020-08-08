261e40b20dc430821ab86fdcc6ce55c0e12899ac
Conrad Buys <conradbuys11@gmail.com>
2020-08-08 14:41:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,11 +8,28 @@ def loop_message_five_times(string)
   end
 end
 
-def loop_message_n_times
+def loop_message_n_times(string, integer)
+  counter = integer
+  while counter > 0 do
+    puts string
+    counter -= 1
+  end
 end
 
-def output_array
+def output_array(array)
+  counter = 0
+  while counter < array.length do
+    puts array[counter]
+    counter += 1
+  end
 end
 
-def return_string_array
+def return_string_array(array)
+  new_array = []
+  counter = 0
+  while counter < array.length do
+    new_array.push(array[counter].to_s)
+    counter += 1
+  end
+  new_array
 end
